280242510157bafe6553da0226f4068a86ffc563
duy.tranphuongkhang@haravan.com <duy.tranphuongkhang@haravan.com>
2021-01-19 17:18:49 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,28 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
 func checknum() {
 	var num string
-	fmt.Print("Enter Binary Number:")
-	fmt.Scanln(&num)
-	output, err := strconv.ParseInt(num, 2, 64)
-	if err != nil {
-		fmt.Println(err)
-		return
+	for {
+		fmt.Print("Enter Binary Number:")
+		fmt.Scanln(&num)
+		output, err := strconv.ParseInt(num, 2, 64)
+		if err == nil {
+			fmt.Printf("Output: %d", output)
+			os.Exit(0)
+		}
+		if err != nil {
+			fmt.Println("invalid input")
+		}
 	}
-	fmt.Printf("Output %d", output)
+
 }
 
 func main() {
-	fmt.Println("Press 1 to run")
-	fmt.Println("Press 2 to exit")
-	var input int
-	n, err := fmt.Scanln(&input)
-	if n < 1 || err != nil {
-		fmt.Println("invalid input")
-		return
+	for {
+		fmt.Println("Press 1 to run")
+		fmt.Println("Press 2 to exit")
+		var input int
+		fmt.Scanln(&input)
+		if input < 1 {
+			fmt.Println("invalid input")
+		} else if input > 2 {
+			fmt.Println("invalid input")
+		} else if input == 1 {
+			checknum()
+		} else {
+			break
+		}
 	}
+
 }
